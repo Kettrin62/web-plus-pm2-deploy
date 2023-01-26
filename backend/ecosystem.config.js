1968a985f78a06cf07085d682a7c8582be3c19e5
Kettrin62 <kettykazakova@yandex.ru>
2023-01-26 20:49:40 +0300

    feat: env/config

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF = 'origin/develop',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'app',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': `cd ${DEPLOY_PATH} && npm i && npm run build`,
+    },
+  },
+}
